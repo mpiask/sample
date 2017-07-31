77f1897eb549d7124853ad227251cce76f88c3c9
mpiask <mpiask@gmail.com>
2017-07-31 15:44:19 +0200

    finished user actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,11 @@
 module SessionsHelper
+
+  # Logs in the given user.
 	def log_in(user)
 		session[:user_id] = user.id
 	end
+
+  # Returns the user corresponding to the remember token cookie.
 	def current_user
 		if (user_id = session[:user_id])
 		  @current_user ||= User.find_by(id: user_id)
@@ -13,26 +17,48 @@ module SessionsHelper
 		  end
 		end
 	end
+
+	# Returns true if the user is logged in
 	def logged_in?
 		!current_user.nil?
 	end
-	def log_out
-  	session.delete(:user_id)
-  	@current_user = nil
-  end
+
+  # Creates permanent cookie, assigns token
   def remember(user)
   	user.remember
   	cookies.permanent.signed[:user_id] = user.id
   	cookies.permanent[:remember_token] = user.remember_token
   end
+
+  # Deletes user's cookie
   def forget(user)
   	user.forget
   	cookies.delete(:user_id)
   	cookies.delete(:remember_token)
   end
+
+  # Logs user out
   def log_out
   	forget(current_user)
   	session.delete(:user_id)
   	@current_user = nil
   end
+
+
+  # Returns true if the given user is the current user.
+  def current_user?(user)
+    user == current_user
+  end
+
+  # Redirects to stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+
 end
