05f1b14a2b06ccf112ce9fc99444ecf43b66b838
mpiask <mpiask@gmail.com>
2017-08-02 15:12:42 +0200

    account activation created

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,9 @@ User.create!(name:  "Example User",
              email: "maciej@maciej.maciej",
              password:              "maciej",
              password_confirmation: "maciej",
-             admin: true)
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 99.times do |n|
   name  = Faker::Name.name
@@ -18,5 +20,7 @@ User.create!(name:  "Example User",
   User.create!(name:  name,
                email: email,
                password:              password,
-               password_confirmation: password)
+               password_confirmation: password,
+	             activated: true,
+	             activated_at: Time.zone.now)
 end
