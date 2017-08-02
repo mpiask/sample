2edc37f0403b2706fa0a6dafe35fa7500ed6ec97
mpiask <mpiask@gmail.com>
2017-08-02 19:31:26 +0200

    working microposts interface

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy
 	attr_accessor :remember_token, :activation_token, :reset_token
 
   before_save :downcase_email
@@ -65,6 +66,12 @@ class User < ApplicationRecord
     reset_sent_at < 2.hours.ago
   end
 
+  # Defines a proto-feed.
+  # See "Following users" for the full implementation.
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+  
   private
 
 	  def downcase_email
